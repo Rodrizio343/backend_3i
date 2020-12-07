0c14efcd232f17b0d9dd3d62fba9aabc815e2d64
Rodrigo Martínez <rodrigozoot94@gmail.com>
2020-12-07 16:50:20 -0300

    Creando archivo config

---
+++ server/config/config.js
@@ -0,0 +1,4 @@
+//=========PUERTO============
+//proporciona info de todos los procesos que estan funcionando en node, por ejemplo el puerto en el cual esta el servidor
+//con esta sentencia defino la info del puerto cuando el proyecto este en produccion y/o en su defecto indico el puerto que empleara cuado este corriendo de forma local
+process.env.PORT=process.env.PORT || 3005
--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
+//para importar, los require siempre arriba
+require('./config/config')
+
 const express = require("express");
+const bodyParser= require("body-parser")
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/usuario', function(req, res) { //req(solicitud) / res(respuesta)
     res.json({
@@ -8,12 +15,24 @@ app.get('/usuario', function(req, res) { //req(solicitud) / res(respuesta)
 });
 
 app.post('/usuario', function(req, res) { //req(solicitud) / res(respuesta)
-    res.json({
-        message:'POST usuario'
-    });
+    
+    let body = req.body
+
+    if(body.nombre===undefined){
+        res.status(400).json({
+            ok:false,
+            message: "El nombre es necesario",
+        })
+    } else{
+        res.json({
+            // message:'POST usuario'
+            persona:body
+        });
+    }
+    
 });
 
-app.put('/usuario', function(req, res) { //req(solicitud) / res(respuesta)
+app.put('/usuario/:id', function(req, res) { //req(solicitud) / res(respuesta)
     res.json({
         message:'PUT usuario'
     });
@@ -25,6 +44,6 @@ app.delete('/usuario', function(req, res) { //req(solicitud) / res(respuesta)
     });
 });
 
-app.listen(3000, ()=>{
-    console.log('Servidor online en puerto ', 3000)
+app.listen(process.env.PORT, ()=>{
+    console.log('Servidor online en puerto ', process.env.PORT)
 })
